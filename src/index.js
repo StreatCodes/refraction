7badebf26b70c0b0f639ec6eced45e82a0391a50
Matthew Streatfield <matt.streatfield@sol1.com.au>
2018-04-20 17:03:26 +1000

    un retardify everything ditch webpack build system works well now and auto loads

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-import style from './style.css'
-console.log('test');
+// dependencies
+const fs = require("fs");
+const postcss = require("postcss");
+const atImport = require("postcss-import");
+const autoprefixer = require('autoprefixer');
+const variables = require('postcss-css-variables');
+const nanocss = require('cssnano');
+const prod = process.env.NODE_ENV === 'production';
+
+// css to be processed
+const css = fs.readFileSync("src/main.css", "utf8");
+
+const plugins = [atImport, variables, autoprefixer];
+let output = 'dist/bundle.css';
+
+if(prod) {
+	plugins.push(nanocss);
+	output = 'dist/bundle.min.css';
+}
+
+// process css
+postcss(plugins)
+	.process(css, { from: "src/main.css", to: output})
+	.then(result => {
+		fs.writeFileSync(output, result.css);
+	});
