14390b8312cb492dfd71bbe04c0ec49385d1799b
StreatCodes <matt.r.streatfield@gmail.com>
2018-04-30 22:47:06 +1000

    Fix text color Make nav float Different code style Better prism

--- helper.js
+++ helper.js
@@ -1,10 +1,7 @@
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
-window.addEventListener('load', () => {
-    const codeBlocks = document.getElementsByClassName('html-ify');
-    for(const block of codeBlocks) {
-        block.innerHTML = htmlEntities(block.innerHTML);
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 0){
+        document.getElementById('nav').classList.add('floating');
+    } else {
+        document.getElementById('nav').classList.remove('floating');
     }
 });
