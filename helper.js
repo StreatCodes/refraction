c76db811367b3ee80adbbc21a11d8462eee9d9f3
StreatCodes <matt.r.streatfield@gmail.com>
2018-05-06 15:51:21 +1000

    Initial header Initial Scrollspy

--- helper.js
+++ helper.js
@@ -1,7 +1,13 @@
 window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 0){
-        document.getElementById('nav').classList.add('floating');
-    } else {
-        document.getElementById('nav').classList.remove('floating');
-    }
+	if (window.pageYOffset > 0) {
+		document.getElementById('nav').classList.add('floating');
+	} else {
+		document.getElementById('nav').classList.remove('floating');
+	}
+
+	const spies = document.querySelectorAll('[spy]');
+
+	for(let spy of spies) {
+		console.log(spy.getAttribute('spy'));
+	}
 });
